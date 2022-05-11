2d79b54d606a7aefc01b2f9e1b16931183c9b869
JC <u202019558@upc.edu.pe>
2022-05-11 17:45:10 -0500

    chore(UI): Added pending ui compnts and primeflex

--- src/learning/pages/tutorial-list.component.vue
+++ src/learning/pages/tutorial-list.component.vue
@@ -3,8 +3,155 @@
 </template>
 
 <script>
+import { TutorialApiService } from "../services/tutorial-api.service";
+import { FilterMatchMode } from "primevue/api";
+
 export default {
   name: "tutorial-list",
+  data() {
+    return {
+      tutorials: [],
+      tutorialDialog: false,
+      deleteTutorialDialog: false,
+      deleteTutorialsDialog: false,
+      tutorial: {},
+      selectedTutorials: null,
+      filters: {},
+      submitted: false,
+      statuses: [
+        { label: "Published", value: true },
+        { label: "Unpublished", value: false },
+      ],
+      tutorialsService: null,
+    };
+  },
+  created() {
+    this.tutorialsService = new TutorialApiService();
+    this.tutorialsService.getAll().then((response) => {
+      this.tutorials = response.data;
+      this.tutorials.forEach(
+        (tutorial) =>
+          (tutorial.published = tutorial.published
+            ? "Published"
+            : "Unpublished")
+      );
+      console.log("created");
+    });
+    //TODO: Init Filters
+    this.initFilters();
+  },
+  methods: {
+    getDisplayableTutorial(tutorial) {
+      tutorial.status = tutorial.published
+        ? this.statuses[0].label
+        : this.statuses[1].label;
+      return tutorial;
+    },
+
+    getStorableTutorial(displayableTutorial) {
+      return {
+        id: displayableTutorial.id,
+        title: displayableTutorial.title,
+        description: displayableTutorial.description,
+        published: displayableTutorial.status.label === "Published",
+      };
+    },
+
+    openNew() {
+      this.tutorial = {};
+      this.submitted = false;
+      this.tutorialDialog = true;
+    },
+    hideDialog() {
+      this.tutorialDialog = false;
+      this.submitted = false;
+    },
+    findIndexById(id) {
+      return this.tutorials.findIndex((tutorial) => tutorial.id === id);
+    },
+    saveTutorial() {
+      this.submitted = true;
+      if (this.tutorial.title.trim()) {
+        if (this.tutorial.id) {
+          this.tutorial = this.getStorableTutorial(this.tutorial);
+          this.tutorialsService
+            .update(this.tutorial.id, this.tutorial)
+            .then((response) => {
+              this.tutorials[this.findIndexById(response.data.id)] =
+                this.getDisplayableTutorial(response.data);
+              this.$toast.add({
+                severity: "success",
+                summary: "Successful",
+                detail: "Tutorial Updated",
+                life: 3000,
+              });
+              console.log(response);
+            });
+        } else {
+          this.tutorial.id = 0;
+          console.log(this.tutorial);
+          this.tutorial = this.getStorableTutorial(this.tutorial);
+          this.tutorialsService.created(this.tutorial).then((response) => {
+            this.tutorial = this.getDisplayableTutorial(response.data);
+            this.tutorials.push(this.tutorial);
+            this.$toast.add({
+              severity: "success",
+              summary: "Successful",
+              detail: "Tutorial Created",
+              life: 3000,
+            });
+            console.log(response);
+          });
+        }
+        this.tutorialDialog = false;
+        this.tutorial = {};
+      }
+    },
+    editTutorial(tutorial) {
+      console.log(tutorial);
+      this.tutorial = { ...tutorial };
+      console.log(this.tutorial);
+      this.deleteTutorialDialog = true;
+    },
+    deleteTutorial() {
+      this.tutorialsService.delete(this.tutorial.id).then((response) => {
+        this.tutorials = this.tutorials.filter(
+          (t) => t.id !== this.tutorial.id
+        );
+        this.deleteTutorialDialog = false;
+        this.tutorial = {};
+        this.$toast.add({
+          severity: "success",
+          summary: "Successful",
+          detail: "Tutorial Deleted",
+          life: 3000,
+        });
+        console.log(response);
+      });
+    },
+    exportToCSV() {
+      this.$refs.dt.exportToCSV();
+    },
+    confirmDeleteSelected() {
+      this.deleteTutorialsDialog = true;
+    },
+    deleteSelectedTutorials() {
+      this.selectedTutorials.forEach((tutorial) => {
+        this.tutorialsService.delete(tutorial.id).then((response) => {
+          this.tutorials = this.tutorials.filter(
+            (t) => t.id !== this.tutorial.id
+          );
+          console.log(response);
+        });
+      });
+      this.deleteTutorialsDialog = false;
+    },
+    initFilters() {
+      this.filters = {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      };
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -8,11 +8,40 @@ import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/themes/bootstrap4-dark-blue/theme.css";
 import "primeicons/primeicons.css";
 
+import Dialog from "primevue/dialog";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Sidebar from "primevue/sidebar";
+import Row from "primevue/row";
+import Toast from "primevue/toast";
+import Dropdown from "primevue/dropdown";
+import Card from "primevue/card";
+import Tag from "primevue/tag";
+import ToastService from "primevue/toastservice";
+
 const app = createApp(App);
 
 app.use(router);
 
 //PrimeVue Config
 app.use(PrimeVue, { ripple: true });
+
+app.use(ToastService);
+
+app.component("pv-data-table", DataTable);
+app.component("pv-column", Column);
+app.component("pv-button", Button);
+app.component("pv-toolbar", Toolbar);
+app.component("pv-input-text", InputText);
+app.component("pv-sidebar", Sidebar);
+app.component("pv-row", Row);
+app.component("pv-dialog", Dialog);
+app.component("pv-toast", Toast);
+app.component("pv-dropdown", Dropdown);
+app.component("pv-tag", Tag);
+app.component("pv-card", Card);
 
 app.mount("#app");
